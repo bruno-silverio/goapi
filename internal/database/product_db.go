18c3196c629406107669bfb746e4149ed42d5969
Bruno Silverio <bcamilo94@gmail.com>
2024-01-31 11:58:06 -0300

    fix: query mysql

--- internal/database/product_db.go
+++ internal/database/product_db.go
@@ -16,7 +16,7 @@ func NewProductDB(db *sql.DB) *ProductDB {
 
 //Listagem de categories
 func (pd *ProductDB) GetProducts() ([]*entity.Product, error) {
-	rows, err := pd.db.Query("SELECT id, name, price, category_id FROM products")
+	rows, err := pd.db.Query("SELECT id, name, description, price, category_id, image_url FROM products")
 	if err != nil {
 		return nil, err
 	}
@@ -25,7 +25,7 @@ func (pd *ProductDB) GetProducts() ([]*entity.Product, error) {
 	var products []*entity.Product
 	for rows.Next() {
 		var product entity.Product
-		if err := rows.Scan(&product.ID, &product.Name, &product.Price, &product.CategoryID); err != nil {
+		if err := rows.Scan(&product.ID, &product.Name, &product.Description, &product.Price, &product.CategoryID, &product.ImageURL); err != nil {
 			return nil, err
 		}
 		products = append(products, &product)
@@ -35,7 +35,7 @@ func (pd *ProductDB) GetProducts() ([]*entity.Product, error) {
 
 func (pd *ProductDB) GetProduct(id string) (*entity.Product, error) {
 	var product entity.Product
-	err := pd.db.QueryRow("SELECT id, name, price, category_id, image_url FROM products WHERE id = ?", id).Scan(&product.ID, &product.Name, &product.Price, &product.CategoryID, &product.ImageURL)
+	err := pd.db.QueryRow("SELECT id, name, description, price, category_id, image_url FROM products WHERE id = ?", id).Scan(&product.ID, &product.Name, &product.Description, &product.Price, &product.CategoryID, &product.ImageURL)
 	if err != nil {
 		return nil, err
 	}
